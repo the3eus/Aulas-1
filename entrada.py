50f4e1397a639c5194c6401c8b92756c83d0bb19
Your Name <you@example.com>
2026-02-12 11:07:08 -0300

    atualização de entrada

---
+++ entrada.py
@@ -0,0 +1,12 @@
+usuario = input('Digite seu nome de usuario ')
+senha = input('Senha ')
+dominio = input('qual seria o dominio? ')
+email = usuario+'@'+dominio)
+
+print ('Olá',usuario, 'seja bem vindo!')
+print ('Sua enha é',senha ,'esta e uma boa senha!')
+print ('seu email é',usuario,'@',dominio,' está correto?')
+
+
+
+
